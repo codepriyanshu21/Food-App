098abbca74cc838c4938294db33d14c4f25a0b61
kingpriyanshu21 <priyanshusingh8233@gmail.com>
2025-01-09 11:59:44 +0530

    save changes

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -2,7 +2,7 @@
 const Shimmer = () => {
     return (
         <div className="p-20 mt-20">
-            <div className="grid grid-cols-1 lg:grid-cols-4 gap-6 -ml-2 lg:ml-12">
+            <div className="grid grid-cols-1 lg:grid-cols-4 gap-6 -ml-4 lg:ml-12">
                 {Array.from({ length: 8 }).map((_, index) => (
                     <div key={index} className="shimmer rounded-lg shadow-lg transform hover:scale-105 transition-transform duration-300"></div>
                 ))}
