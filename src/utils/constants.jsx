a58aee72ca878da394f84ce11bfa8156da8bb770
kingpriyanshu21 <priyanshusingh8233@gmail.com>
2025-01-20 11:41:01 +0530

    changes made successfully

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -2,7 +2,7 @@ export const LOGO_URL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGp
 
 export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
 
-export const MENU_API="https://proxy.cors.sh/https://www.swiggy.com/dapi/menu/pl";
+export const MENU_API="https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl";
 
 export const IMAGE_URL="FOOD_CATALOG/IMAGES/CMS/2024/10/23/db150a3a-6f10-4781-accc-4913a6ffebba_ead68be3-70f9-4b43-9b9d-69b854117b74.png"
 
@@ -10,4 +10,4 @@ export const COOKING_IMAGE="https://img95.lovepik.com/photo/40105/7176.gif_wh860
 
 export const BACKGROUND_IMAGE="https://static.vecteezy.com/system/resources/previews/037/245/808/non_2x/ai-generated-beautuful-fast-food-background-with-copy-space-free-photo.jpg"
 
-export const RESTAURANTS_LIST="https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.2014149&lng=76.8275503&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const RESTAURANTS_LIST="https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.2014149&lng=76.8275503&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
